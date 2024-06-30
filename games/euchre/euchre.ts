e7623ac1151944a5d1fd750ad38cb954bdbfe693
Rich McLaughlin <rsmclaug@gmail.com>
2024-06-30 15:07:14 -0400

    set trump on the deck

--- games/euchre/euchre.ts
+++ games/euchre/euchre.ts
@@ -31,6 +31,11 @@ interface Player {
     isPlayerTeammate: boolean;
 }
 
+interface Team {
+    players: Player[];
+    score: number;
+}
+
 const isCardTrump = (card: Card, trump: Suit): boolean => {
     if (card.suit === trump) {
         return true;
@@ -77,10 +82,9 @@ const shuffleDeck = (deck: Card[]) => {
  * Deal 5 cards to each player, and create the kitty with the remaining 4 cards
  */
 const dealDeck = (deck: Card[], players: Player[]) => {
-    for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < players.length; j++) {
-            players[j].hand.push(deck[i + j]);
-        }
+    // dealt the first 20 cards randomly to the players so that each player gets 5 cards
+    for (let i = 0; i < 20; i++) {
+        players[i % 4].hand.push(deck[i]);
     }
 
     const kitty = deck.slice(20, 24);
@@ -123,22 +127,76 @@ const createPlayers = (): Player[] => {
     ];
 };
 
+const logger = (label = '', obj: any): void => {
+    // console.log(label, JSON.stringify(obj, null, 2));
+    console.log(label);
+    console.dir(obj, { depth: null, colors: true });
+};
+
 class Game {
     public deck: Card[];
     public players: Player[];
+    public teams: Team[];
     public kitty: Card[];
+    public trump: Suit;
 
     constructor() {
         this.deck = shuffleDeck(buildDeck());
         this.players = createPlayers();
+        this.teams = [
+            {
+                players: [this.players[0], this.players[2]],
+                score: 0
+            },
+            {
+                players: [this.players[1], this.players[3]],
+                score: 0
+            }
+        ];
         this.kitty = dealDeck(this.deck, this.players);
+
+        //set trump to the top card of the kitty
+        this.trump = this.kitty[0].suit;
+
+        this.startGame();
+    }
+
+    public startGame() {
+        const dealerIndex = Math.floor(Math.random() * 4);
+        this.players[dealerIndex].isDealer = true;
+
+        // update the isTrump property for each card in the players' hands
+        this.setTrumpOnDeck();
+
+        logger('trump', this.trump);
+        logger('players', this.players);
+    }
+
+    public setTrumpOnDeck() {
+        this.players.forEach(player => {
+            player.hand.forEach(card => {
+                card.isTrump = isCardTrump(card, this.trump);
+            });
+        });
+    }
+
+    public printPlayerTeams(players: Player[]) {
+        const playerTeams = players.map(player => {
+            return {
+                team: player.team,
+                isPlayer: player.isPlayer,
+                isDealer: player.isDealer,
+                isPlayerTeammate: player.isPlayerTeammate
+            };
+        });
+
+        logger('playerTeams', playerTeams);
     }
 }
 
 (() => {
     const game = new Game();
 
-    // console.log('deck', game.deck);
-    console.log('players', game.players);
-    console.log('kitty', game.kitty);
+    // logger('players', game.players);
+    // logger('kitty', game.kitty);
 })();
