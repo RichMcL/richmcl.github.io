166d1e75a881d9e0570a1f4821327ba664d32387
Rich McLaughlin <rsmclaug@gmail.com>
2024-07-07 16:09:32 -0400

    starting game board layout

--- games/euchre/scripts/layout-grid.js
+++ games/euchre/scripts/layout-grid.js
@@ -1,14 +1,30 @@
 (() => {
     function createVLineElement(value) {
         const div = document.createElement('div');
-        div.className = `layout-grid-vline v-${value}`;
+        div.className = `layout-grid-vline`;
         div.style.left = `${value}px`;
         return div;
     }
 
     function createHLineElement(value) {
         const div = document.createElement('div');
-        div.className = `layout-grid-hline h-${value}`;
+        div.className = `layout-grid-hline`;
+        div.style.top = `${value}px`;
+        return div;
+    }
+
+    function createVLineLabelElement(value) {
+        const div = document.createElement('div');
+        div.innerText = value;
+        div.className = 'layout-grid-vlabel';
+        div.style.left = `${value}px`;
+        return div;
+    }
+
+    function createHLineLabelElement(value) {
+        const div = document.createElement('div');
+        div.innerText = value;
+        div.className = `layout-grid-hlabel`;
         div.style.top = `${value}px`;
         return div;
     }
@@ -19,13 +35,23 @@
         for (let i = 0; i <= 1280; i += 10) {
             const vLineElement = createVLineElement(i);
             container.appendChild(vLineElement);
+
+            if (i % 100 === 0) {
+                const labelElement = createVLineLabelElement(i);
+                container.appendChild(labelElement);
+            }
         }
 
         for (let i = 0; i <= 800; i += 10) {
             const hLineElement = createHLineElement(i);
             container.appendChild(hLineElement);
+
+            if (i % 100 === 0) {
+                const labelElement = createHLineLabelElement(i);
+                container.appendChild(labelElement);
+            }
         }
     }
 
-    setTimeout(() => doIt(), 1000);
+    setTimeout(() => doIt(), 100);
 })();
