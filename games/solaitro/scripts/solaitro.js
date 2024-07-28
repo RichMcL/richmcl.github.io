2a1b158bd5bbc1aca7ceb3b964dbab97fac64e61
Rich McLaughlin <rsmclaug@gmail.com>
2024-07-28 17:38:24 -0400

    switch to canvas, render full deck

--- games/solaitro/scripts/solaitro.js
+++ games/solaitro/scripts/solaitro.js
@@ -89,15 +89,30 @@ var Game = /** @class */ (function () {
         this.timerInMs = 0;
         this.lastTimestamp = 0;
         this.piles = [];
-        this.handlePileZoneClick = function (event) {
-            console.log('clicked at', _this.timerInMs);
-        };
-        this.deck = this.buildAndShuffleDeck();
-        this.startGame();
+        this.canvas = document.getElementById('game-canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.cardFaceSpriteSheet = new Image();
+        this.cardFaceSpriteSheet.src = 'img/deck-sprite-sheet.png';
+        this.cardBackSpriteSheet = new Image();
+        this.cardBackSpriteSheet.src = 'img/card-backs-seals.png';
+        // Wait for both images to load before starting the game
+        Promise.all([
+            this.loadImage(this.cardFaceSpriteSheet),
+            this.loadImage(this.cardBackSpriteSheet)
+        ]).then(function () {
+            _this.startGame();
+        });
     }
+    Game.prototype.loadImage = function (image) {
+        return new Promise(function (resolve) {
+            image.onload = function () { return resolve(); };
+        });
+    };
     Game.prototype.startGame = function () {
         console.log('Game started', this);
-        this.initializePileZones(4);
+        // this.initializePileZones(4);
+        this.deck = this.buildAndShuffleDeck();
+        console.log('Deck', this.deck);
         this.gameRunning = true;
         this.lastTimestamp = performance.now();
         this.gameLoop();
@@ -123,9 +138,16 @@ var Game = /** @class */ (function () {
     };
     Game.prototype.render = function () {
         // Render the updated state to the DOM
-        this.renderPileZones();
-        this.renderPlayerDeck();
-        this.renderTimer();
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Clear canvas
+        // columns -> 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+        // rows -> hearts, clubs, diamonds, spades,
+        var cardIndex = 0;
+        for (var col = 0; col < 4; col++) {
+            for (var row = 0; row < 13; row++) {
+                this.drawCard(this.deck[cardIndex], row * 71, col * 95);
+                cardIndex++;
+            }
+        }
     };
     Game.prototype.pauseGame = function () {
         this.gameRunning = false;
@@ -140,28 +162,85 @@ var Game = /** @class */ (function () {
                 deck.push({ suit: suit, value: value });
             }
         }
-        for (var i = 0; i < deck.length; i++) {
-            var j = Math.floor(Math.random() * deck.length);
-            var temp = deck[i];
-            deck[i] = deck[j];
-            deck[j] = temp;
-        }
+        //Shuffle deck
+        // for (let i = 0; i < deck.length; i++) {
+        //     const j = Math.floor(Math.random() * deck.length);
+        //     const temp = deck[i];
+        //     deck[i] = deck[j];
+        //     deck[j] = temp;
+        // }
         return deck;
     };
     Game.prototype.initializePileZones = function (count) {
         this.piles = this.deck.splice(0, count);
     };
     /* USER ACTION FUNCTIONS */
-    Game.prototype.clickDeck = function () {
-        //on click of the deck, increment the deck position
-        this.deckPosition++;
-    };
     /* RENDER FUNCTIONS */
-    Game.prototype.buildCardHtml = function (card) {
-        return "\n        <div class=\"card-wrapper\">\n            <div class=\"card-face ".concat(cardValueToKey(card.value), "-").concat(card.suit.toLowerCase(), "\"></div>\n        </div>\n        ");
+    Game.prototype.drawCard = function (card, x, y) {
+        var cardWidth = 71; // Width of a single card in the sprite sheet
+        var cardHeight = 95; // Height of a single card in the sprite sheet
+        var cardIndex = 0;
+        var sy = 0;
+        var sx = 0;
+        switch (card.suit) {
+            case Suit.Hearts:
+                sy = cardHeight * 0;
+                break;
+            case Suit.Clubs:
+                sy = cardHeight * 1;
+                break;
+            case Suit.Diamonds:
+                sy = cardHeight * 2;
+                break;
+            case Suit.Spades:
+                sy = cardHeight * 3;
+                break;
+        }
+        switch (card.value) {
+            case CardValue.Two:
+                sx = cardWidth * 0;
+                break;
+            case CardValue.Three:
+                sx = cardWidth * 1;
+                break;
+            case CardValue.Four:
+                sx = cardWidth * 2;
+                break;
+            case CardValue.Five:
+                sx = cardWidth * 3;
+                break;
+            case CardValue.Six:
+                sx = cardWidth * 4;
+                break;
+            case CardValue.Seven:
+                sx = cardWidth * 5;
+                break;
+            case CardValue.Eight:
+                sx = cardWidth * 6;
+                break;
+            case CardValue.Nine:
+                sx = cardWidth * 7;
+                break;
+            case CardValue.Ten:
+                sx = cardWidth * 8;
+                break;
+            case CardValue.Jack:
+                sx = cardWidth * 9;
+                break;
+            case CardValue.Queen:
+                sx = cardWidth * 10;
+                break;
+            case CardValue.King:
+                sx = cardWidth * 11;
+                break;
+            case CardValue.Ace:
+                sx = cardWidth * 12;
+                break;
+        }
+        this.ctx.drawImage(this.cardBackSpriteSheet, 71, 0, cardWidth, cardHeight, x, y, cardWidth, cardHeight);
+        this.ctx.drawImage(this.cardFaceSpriteSheet, sx, sy, cardWidth, cardHeight, x, y, cardWidth, cardHeight);
     };
     Game.prototype.renderTimer = function () {
-        var timerEl = document.querySelector('.timer-value');
         var minutes = Math.floor(this.timerInMs / 60000)
             .toString()
             .padStart(2, '0');
@@ -171,26 +250,6 @@ var Game = /** @class */ (function () {
         var tenths = Math.floor((this.timerInMs % 1000) / 100)
             .toString()
             .padStart(1, '0');
-        timerEl.innerHTML = "".concat(minutes, ":").concat(seconds, ".").concat(tenths);
-    };
-    Game.prototype.renderPileZones = function () {
-        var _this = this;
-        var pileZonesEl = document.querySelector('.pile-zones');
-        pileZonesEl.innerHTML = '';
-        this.piles.forEach(function (card, index) {
-            var cardHtml = _this.buildCardHtml(card);
-            pileZonesEl.innerHTML += cardHtml;
-        });
-        //remove existing click event listeners
-        pileZonesEl.removeEventListener('click', this.handlePileZoneClick);
-        pileZonesEl.addEventListener('click', this.handlePileZoneClick);
-    };
-    Game.prototype.renderPlayerDeck = function () {
-        var playerDeckEl = document.querySelector('.player-deck');
-        // Render the current position of the deck
-        var topCard = this.deck[this.deckPosition];
-        var topCardHtml = this.buildCardHtml(topCard);
-        playerDeckEl.innerHTML = topCardHtml;
     };
     // Sleep function using Promise and async/await
     Game.prototype.sleep = function (ms) {
--- games/solaitro/solaitro.ts
+++ games/solaitro/solaitro.ts
@@ -102,6 +102,11 @@ const cardValueToKey = (value: CardValue): string => {
 };
 
 class Game {
+    public canvas: HTMLCanvasElement;
+    public ctx: CanvasRenderingContext2D;
+    public cardFaceSpriteSheet: HTMLImageElement;
+    public cardBackSpriteSheet: HTMLImageElement;
+
     public gameRunning: boolean = true;
     public deck: Card[];
     public deckPosition: number = 0;
@@ -111,15 +116,38 @@ class Game {
     public piles: Card[] = [];
 
     constructor() {
-        this.deck = this.buildAndShuffleDeck();
+        this.canvas = document.getElementById('game-canvas') as HTMLCanvasElement;
+        this.ctx = this.canvas.getContext('2d');
 
-        this.startGame();
+        this.cardFaceSpriteSheet = new Image();
+        this.cardFaceSpriteSheet.src = 'img/deck-sprite-sheet.png';
+
+        this.cardBackSpriteSheet = new Image();
+        this.cardBackSpriteSheet.src = 'img/card-backs-seals.png';
+
+        // Wait for both images to load before starting the game
+        Promise.all([
+            this.loadImage(this.cardFaceSpriteSheet),
+            this.loadImage(this.cardBackSpriteSheet)
+        ]).then(() => {
+            this.startGame();
+        });
+    }
+
+    private loadImage(image: HTMLImageElement): Promise<void> {
+        return new Promise(resolve => {
+            image.onload = () => resolve();
+        });
     }
 
     public startGame() {
         console.log('Game started', this);
 
-        this.initializePileZones(4);
+        // this.initializePileZones(4);
+
+        this.deck = this.buildAndShuffleDeck();
+
+        console.log('Deck', this.deck);
 
         this.gameRunning = true;
         this.lastTimestamp = performance.now();
@@ -151,9 +179,17 @@ class Game {
 
     public render() {
         // Render the updated state to the DOM
-        this.renderPileZones();
-        this.renderPlayerDeck();
-        this.renderTimer();
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Clear canvas
+
+        // columns -> 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+        // rows -> hearts, clubs, diamonds, spades,
+        let cardIndex = 0;
+        for (let col = 0; col < 4; col++) {
+            for (let row = 0; row < 13; row++) {
+                this.drawCard(this.deck[cardIndex], row * 71, col * 95);
+                cardIndex++;
+            }
+        }
     }
 
     public pauseGame() {
@@ -171,12 +207,14 @@ class Game {
             }
         }
 
-        for (let i = 0; i < deck.length; i++) {
-            const j = Math.floor(Math.random() * deck.length);
-            const temp = deck[i];
-            deck[i] = deck[j];
-            deck[j] = temp;
-        }
+        //Shuffle deck
+
+        // for (let i = 0; i < deck.length; i++) {
+        //     const j = Math.floor(Math.random() * deck.length);
+        //     const temp = deck[i];
+        //     deck[i] = deck[j];
+        //     deck[j] = temp;
+        // }
 
         return deck;
     }
@@ -187,24 +225,99 @@ class Game {
 
     /* USER ACTION FUNCTIONS */
 
-    public clickDeck(): void {
-        //on click of the deck, increment the deck position
-        this.deckPosition++;
-    }
-
     /* RENDER FUNCTIONS */
 
-    public buildCardHtml(card: Card): string {
-        return `
-        <div class="card-wrapper">
-            <div class="card-face ${cardValueToKey(card.value)}-${card.suit.toLowerCase()}"></div>
-        </div>
-        `;
+    private drawCard(card: Card, x: number, y: number): void {
+        const cardWidth = 71; // Width of a single card in the sprite sheet
+        const cardHeight = 95; // Height of a single card in the sprite sheet
+        const cardIndex = 0;
+
+        let sy = 0;
+        let sx = 0;
+
+        switch (card.suit) {
+            case Suit.Hearts:
+                sy = cardHeight * 0;
+                break;
+            case Suit.Clubs:
+                sy = cardHeight * 1;
+                break;
+            case Suit.Diamonds:
+                sy = cardHeight * 2;
+                break;
+            case Suit.Spades:
+                sy = cardHeight * 3;
+                break;
+        }
+
+        switch (card.value) {
+            case CardValue.Two:
+                sx = cardWidth * 0;
+                break;
+            case CardValue.Three:
+                sx = cardWidth * 1;
+                break;
+            case CardValue.Four:
+                sx = cardWidth * 2;
+                break;
+            case CardValue.Five:
+                sx = cardWidth * 3;
+                break;
+            case CardValue.Six:
+                sx = cardWidth * 4;
+                break;
+            case CardValue.Seven:
+                sx = cardWidth * 5;
+                break;
+            case CardValue.Eight:
+                sx = cardWidth * 6;
+                break;
+            case CardValue.Nine:
+                sx = cardWidth * 7;
+                break;
+            case CardValue.Ten:
+                sx = cardWidth * 8;
+                break;
+            case CardValue.Jack:
+                sx = cardWidth * 9;
+                break;
+            case CardValue.Queen:
+                sx = cardWidth * 10;
+                break;
+            case CardValue.King:
+                sx = cardWidth * 11;
+                break;
+            case CardValue.Ace:
+                sx = cardWidth * 12;
+                break;
+        }
+
+        this.ctx.drawImage(
+            this.cardBackSpriteSheet,
+            71,
+            0,
+            cardWidth,
+            cardHeight,
+            x,
+            y,
+            cardWidth,
+            cardHeight
+        );
+
+        this.ctx.drawImage(
+            this.cardFaceSpriteSheet,
+            sx,
+            sy,
+            cardWidth,
+            cardHeight,
+            x,
+            y,
+            cardWidth,
+            cardHeight
+        );
     }
 
     public renderTimer(): void {
-        const timerEl = document.querySelector('.timer-value');
-
         const minutes = Math.floor(this.timerInMs / 60000)
             .toString()
             .padStart(2, '0');
@@ -214,35 +327,6 @@ class Game {
         const tenths = Math.floor((this.timerInMs % 1000) / 100)
             .toString()
             .padStart(1, '0');
-
-        timerEl.innerHTML = `${minutes}:${seconds}.${tenths}`;
-    }
-
-    public handlePileZoneClick = (event: Event) => {
-        console.log('clicked at', this.timerInMs);
-    };
-
-    public renderPileZones(): void {
-        const pileZonesEl = document.querySelector('.pile-zones');
-        pileZonesEl.innerHTML = '';
-
-        this.piles.forEach((card, index) => {
-            const cardHtml = this.buildCardHtml(card);
-            pileZonesEl.innerHTML += cardHtml;
-        });
-
-        //remove existing click event listeners
-        pileZonesEl.removeEventListener('click', this.handlePileZoneClick);
-        pileZonesEl.addEventListener('click', this.handlePileZoneClick);
-    }
-
-    public renderPlayerDeck(): void {
-        const playerDeckEl = document.querySelector('.player-deck');
-
-        // Render the current position of the deck
-        const topCard = this.deck[this.deckPosition];
-        const topCardHtml = this.buildCardHtml(topCard);
-        playerDeckEl.innerHTML = topCardHtml;
     }
 
     // Sleep function using Promise and async/await
