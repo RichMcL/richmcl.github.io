7f24f46112ccb0dd4fd8e9ea9ca70b5455376f62
Rich McLaughlin <rsmclaug@gmail.com>
2024-08-21 08:58:02 -0400

    border radius for pile shadow

--- games/solaitro/src/solaitro.ts
+++ games/solaitro/src/solaitro.ts
@@ -533,8 +533,31 @@ export class Game {
     }
 
     public renderPileShadow(): void {
-        this.ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
-        this.ctx.fillRect(330, 70, 620, 180);
+        this.ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+        this.drawRoundedRect(this.ctx, 330, 70, 620, 180, 10);
+        this.ctx.fill();
+    }
+
+    // Function to draw a rounded rectangle
+    public drawRoundedRect(
+        ctx: CanvasRenderingContext2D,
+        x: number,
+        y: number,
+        width: number,
+        height: number,
+        radius: number
+    ) {
+        ctx.beginPath();
+        ctx.moveTo(x + radius, y);
+        ctx.lineTo(x + width - radius, y);
+        ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+        ctx.lineTo(x + width, y + height - radius);
+        ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+        ctx.lineTo(x + radius, y + height);
+        ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+        ctx.lineTo(x, y + radius);
+        ctx.quadraticCurveTo(x, y, x + radius, y);
+        ctx.closePath();
     }
 
     public renderSidebar() {
